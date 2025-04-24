2f42b5912047e3d4d3c8c1f90e095f41dcdaa9f1
Jeongeun Lee <zaekoch@gmail.com>
2025-04-24 19:37:23 +0900

    feat: Header 컴포넌트에 MobileNavDropdown 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,23 @@ import Link from "next/link";
 import Image from "next/image";
 import ThemeToggle from "./ThemeToggle";
 import logo from "../../public/images/logo.svg";
-import { FaGithub, FaInfo } from "react-icons/fa";
-import { FaHashnode } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { FaHashnode, FaBoltLightning } from "react-icons/fa6";
 import { usePathname } from "next/navigation";
+import MobileNavDropdown from "./MobileNavDropdown";
+
+const navItems = [
+  {
+    id: 1,
+    name: "Projects",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "Dictonary",
+    link: "/dict",
+  },
+];
 
 export default function Header() {
   const pathname = usePathname();
@@ -20,28 +34,45 @@ export default function Header() {
       }`}
     >
       <div className="flex items-center justify-between max-w-[1500px] h-[60px] mx-auto px-4">
-        <Link href="/">
-          <div className="relative w-6 md:w-8 aspect-square">
-            <Image src={logo} alt="" fill className="object-cover" />
-          </div>
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/">
+            <div className="relative w-6 md:w-8 aspect-square">
+              <Image src={logo} alt="" fill className="object-cover" />
+            </div>
+          </Link>
+          <MobileNavDropdown />
+        </div>
 
         <div className="flex items-center gap-6">
+          <nav className="hidden md:block">
+            <ul className="flex items-center text-[14px] font-medium">
+              {navItems.map((item) => (
+                <li key={item.id}>
+                  <Link
+                    href={item.link}
+                    className="inline-block px-3 py-1.5 ml-2 rounded-[8px] hover:bg-[var(--hover-background)] opacity-85 hover:opacity-100"
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
           <ThemeToggle />
           <div className="relative flex items-center text-[#3C3C43] dark:text-[#EBEBF5] before:content-[''] before:absolute before:bg-[var(--border-color)] before:w-px before:h-[20px] before:-left-2">
-            <Link
-              href="https://jeongeun.hashnode.dev"
-              target="_blank"
-              className="p-2 rounded-[8px] hover:bg-[var(--hover-background)] opacity-75 hover:opacity-100 cursor-pointer hover:text-[#2862FF]"
-            >
-              <FaInfo size={20} />
-            </Link>
             <Link
               href="https://github.com/jeonoeun"
               target="_blank"
               className="p-2 rounded-[8px] hover:bg-[var(--hover-background)] opacity-75 hover:opacity-100 cursor-pointer hover:text-[#24292F] dark:hover:text-[#EBEBF5]"
             >
               <FaGithub size={20} />
+            </Link>
+            <Link
+              href="https://github.com/jeonoeun"
+              target="_blank"
+              className="p-2 rounded-[8px] hover:bg-[var(--hover-background)] opacity-75 hover:opacity-100 cursor-pointer hover:text-[#FFD63A]"
+            >
+              <FaBoltLightning size={20} />
             </Link>
             <Link
               href="https://jeongeun.hashnode.dev"
