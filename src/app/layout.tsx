1022d4c4efb698a238a1260a6f07757c804dcc33
jeonqeun <zaekoch@gmail.com>
2025-09-26 00:30:22 +0900

    refactor: 레이아웃과 메타데이터 일부 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Header from "@/components/layout/Header";
+import Header from "@/components/layout/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "이정은 (Jeongeun Lee)",
-  description: "개인 프로젝트와 웹 퍼블리싱 작업을 정리한 포트폴리오 사이트",
+  description: "Portfolio of web publisher & frontend developer Jeongeun Lee",
 };
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider attribute="class">
           <Header />
-          <main>{children}</main>
+          {children}
         </ThemeProvider>
       </body>
     </html>
