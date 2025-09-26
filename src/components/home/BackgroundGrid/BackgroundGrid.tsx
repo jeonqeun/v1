95dddb9d89df983f691c265dcab6f110bdb5ec6b
jeonqeun <zaekoch@gmail.com>
2025-09-26 18:14:15 +0900

    fix: BackgroundGrid 색상 rgba로 변경

--- src/components/home/BackgroundGrid/BackgroundGrid.tsx
+++ src/components/home/BackgroundGrid/BackgroundGrid.tsx
@@ -3,7 +3,7 @@ import BeamOverlaySequential from "./BeamOverlaySequential";
 export default function BackgroundGrid() {
   return (
     <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-      <div className="absolute inset-0 bg-[radial-gradient(110%_80%_at_50%_50%,#6B5FAE40,transparent_50%)] blur-3xl" />
+      <div className="absolute inset-0 bg-[radial-gradient(110%_80%_at_50%_50%,rgba(107,95,174,0.25),transparent_50%)] blur-3xl" />
 
       <div className="absolute inset-0 [-webkit-mask-image:radial-gradient(120%_120%_at_50%_50%,#000_32%,transparent_75%)] [mask-image:radial-gradient(120%_120%_at_50%_50%,#000_32%,transparent_75%)]">
         <div className="absolute inset-0 [-webkit-mask-image:linear-gradient(to_bottom,transparent_0%,#000_12%,#000_100%)] [mask-image:linear-gradient(to_bottom,transparent_0%,#000_12%,#000_100%)]">
